888629f55691fbf42e0601279e1290300e97c1cb
Mr.Glum <degetq@gmail.com>
2021-06-16 11:12:35 +0500

    кнопочки...

--- Aiobot.py
+++ Aiobot.py
@@ -1,16 +1,21 @@
 from typing import Text
-
+from aiogram.utils.callback_data import CallbackData
 from aiogram.bot.api import compose_data
 from aiogram.types import chat, message
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import BotCommand
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from contextlib import suppress
+from aiogram.utils.exceptions import MessageNotModified
 
 import config
 import logging
 
 
 API_TOKEN = config.TOKEN
+user_data = {}
+
+
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -30,27 +35,62 @@ vopros_otvet = { "вопрос1": "ответ1",
 
 async def set_commands(bot: Bot):
     commands = [
-        BotCommand(command="/start", description="Запустить бота"),
-        BotCommand(command="/info", description="Краткая справка"),
-        BotCommand(command="/help", description="Позвать кожанного")
+        BotCommand(command="start", description="Запустить бота"),
+        BotCommand(command="info", description="Краткая справка"),
+        BotCommand(command="help", description="Позвать кожанного"),
+        BotCommand(command="numbers_fab", description="Позвать кожанного")
     ]
     await bot.set_my_commands(commands)
 
-# @dp.message_handler(commands="start")
-# async def cmd_start(message: types.Message):
-#     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#     buttons = ["С пюрешкой", "Без пюрешки"]
-#     keyboard.add(*buttons)
-#     await message.answer("Как подавать котлеты?", reply_markup=keyboard)
-
-# @dp.message_handler(Text(equals="С пюрешкой"))
-# async def with_puree(message: types.Message):
-#     await message.reply("Отличный выбор!", reply_markup=types.ReplyKeyboardRemove())
+# fabnum - префикс, action - название аргумента, которым будем передавать значение
+callback_numbers = CallbackData("fabnum", "action")
 
 
-#@dp.message_handler(commands=['start', "start@My_best_aw_bot"], content_types=["Привет", "Привет@My_best_aw_bot"])
-#async def send_welcome(message: types.Message):
-#    await message.reply("Привет! Ты можешь задать вопрос прямо в чате, если тебе нужна справка напиши /info.\nЕсли не помогло напиши /help")
+def get_keyboard_fab():
+    buttons = [
+        types.InlineKeyboardButton(text="-1", callback_data=callback_numbers.new(action="decr")),
+        types.InlineKeyboardButton(text="+1", callback_data=callback_numbers.new(action="incr")),
+        types.InlineKeyboardButton(text="Подтвердить", callback_data=callback_numbers.new(action="finish"))
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttons)
+    return keyboard
+
+
+async def update_num_text_fab(message: types.Message, new_value: int):
+    with suppress(MessageNotModified):
+        await message.edit_text(f"Укажите число: {new_value}", reply_markup=get_keyboard_fab())
+
+
+@dp.message_handler(commands="numbers_fab")
+async def cmd_numbers(message: types.Message):
+    user_data[message.from_user.id] = 0
+    await message.answer("Укажите число: 0", reply_markup=get_keyboard_fab())
+
+
+@dp.callback_query_handler(callback_numbers.filter(action=["incr", "decr"]))
+async def callbacks_num_change_fab(call: types.CallbackQuery, callback_data: dict):
+    user_value = user_data.get(call.from_user.id, 0)
+    action = callback_data["action"]
+    if action == "incr":
+        user_data[call.from_user.id] = user_value + 1
+        await update_num_text_fab(call.message, user_value + 1)
+    elif action == "decr":
+        user_data[call.from_user.id] = user_value - 1
+        await update_num_text_fab(call.message, user_value - 1)
+    await call.answer()
+
+
+@dp.callback_query_handler(callback_numbers.filter(action=["finish"]))
+async def callbacks_num_finish_fab(call: types.CallbackQuery):
+    user_value = user_data.get(call.from_user.id, 0)
+    await call.message.edit_text(f"Итого: {user_value}")
+    await call.answer()
+
+
+@dp.message_handler(commands=['start', "start@My_best_aw_bot"], content_types=["Привет", "Привет@My_best_aw_bot"])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Ты можешь задать вопрос прямо в чате, если тебе нужна справка напиши /info.\nЕсли не помогло напиши /help")
 
 
 @dp.message_handler(commands=['info', "info@My_best_aw_bot"])
