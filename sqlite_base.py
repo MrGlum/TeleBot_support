fb9940daedad71adea8785295400eed108c10080
MrGlum <degetq@gmail.com>
2021-07-11 18:51:03 +0500

    пошарить

--- sqlite_base.py
+++ sqlite_base.py
@@ -3,15 +3,24 @@ import sqlite3
 conn = sqlite3.connect('orders.db')
 cur = conn.cursor()
 
-cur.execute("""CREATE TABLE IF NOT EXISTS users(
-   userid INT PRIMARY KEY,
-   fname TEXT,
-   lname TEXT,
-   gender TEXT);
-""")
 conn.commit()
 
-cur.execute("""INSERT INTO users(userid, fname, lname, gender) 
-   VALUES('00001', 'Alex', 'Smith', 'male');""")
+try:
+   cur.execute("""INSERT INTO users(userid, fname, lname, gender) 
+      VALUES('00001', 'Alex', 'Smith', 'male');""")
+except:
+   print("NOOOOOOO!")
+
+
 conn.commit()
 
+if __name__ == '__main__':
+   try:
+      cur.execute("""CREATE TABLE IF NOT EXISTS users(
+      userid INT PRIMARY KEY,
+      fname TEXT,
+      lname TEXT,
+      gender TEXT);
+      """)
+   except:
+      print("I have this table!")
